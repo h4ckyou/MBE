8ee08a32f5fa4719705e3025fd99edfc18b32281
Mark Uchechukwu <127159644+h4ckyou@users.noreply.github.com>
2024-12-19 21:58:08 +0100

    Update solve_lab1.py
    Solved

--- 1. Reverse Engineering/solve_lab1.py
+++ 1. Reverse Engineering/solve_lab1.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
 
+username = "A"*31
+serial = (ord(username[3]) ^ 0x1337) + 0x5EEDED
+n = len(username)
+
+for i in range(n):
+    serial += (serial ^ ord(username[i])) % 0x539
+
+print(f"Username: {username}")
+print(f"Generated serial: {serial}")
